1d0f6289a4947166f1c1b1f7382cafedf53631fb
Rushikesh Dixit <Rushi@Rushikesh.local>
2017-11-11 14:15:25 -0600

    Added html file

---
+++ templates/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Rushi on 11/10/17.
+ */
+var myVar = setInterval(function(){ myTimer() }, 1000);
+var i=0;
+function myTimer() {
+    i=i%360+1;
+    document.getElementById("demo").innerHTML = i;
+}
